2425185ca859f4523bc1276deed7fb84c686ee41
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-08-13 00:54:20 +0200

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import "../global.scss";
+  import "../app.css";
+</script>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let cat = "cat";
+</script>
+
+<h1 class='text-4xl'>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>alsadig profoloi <a href="https://alsadig-ahmed.netlify.app">alsadig.ahmed.netlify.app</a></p>
+
+<style lang="scss">
+    a{
+        @apply text-blue-700  text-xl;
+        &:hover{
+            @apply text-red-400 ;
+        }
+    }
+  h1 {
+    $v: red;
+    background-color: $v;
+  }
+</style>
